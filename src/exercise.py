f378e8884beb1366255c6089b81f9eca3335bb45
Justin Turner <mrfathed@protonmail.com>
2021-12-04 10:27:31 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    first = int(input("Give the first number:"))
+    second = int(input("Give the second number:"))
+    third = int(input("Give the third number:"))
+
+    average = (first + second + third) / 3
+    print("The average is {}".format(average))
 
 if __name__ == '__main__':
     main()
